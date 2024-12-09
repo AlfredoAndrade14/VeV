c0493a0e12a64ebca6aaa5f9048004fd40112768
Alfredo <alfredo.andrade@ccc.ufcg.edu.br>
2024-12-08 21:20:03 -0300

    feat: first test, create objects

---
+++ lab 1/contas/src/test/java/ProcessadorDeContasTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProcessadorDeContasTest {
+    private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    @Test
+    public void TestCreateEntities() throws ParseException {
+        Fatura fatura = new Fatura("Cliente A", sdf.parse("20/02/2023"), 1500.00);
+        Conta conta = new Conta("001", sdf.parse("20/02/2023"), 500.00);
+
+        assertEquals("Cliente A", fatura.getCliente());
+        assertEquals(sdf.parse("20/02/2023"), fatura.getData());
+        assertEquals(1500.00, fatura.getValor(), 0.01);
+
+        assertEquals("001", conta.getCodigo());
+        assertEquals(sdf.parse("20/02/2023"), conta.getData());
+        assertEquals(500.00, conta.getValorPago(), 0.01);
+    }
+}
