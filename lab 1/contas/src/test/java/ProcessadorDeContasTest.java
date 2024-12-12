779adf89a2947eb1c39e7a5e987bed09bbb8d55d
Alfredo <alfredo.andrade@ccc.ufcg.edu.br>
2024-12-12 07:43:06 -0300

    feat: process

--- lab 1/contas/src/test/java/ProcessadorDeContasTest.java
+++ lab 1/contas/src/test/java/ProcessadorDeContasTest.java
@@ -4,9 +4,29 @@ import com.contas.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProcessadorDeContasTest {
+    private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     
+    @Test
+    public void TestProcessPagamentos() throws ParseException {
+        Fatura fatura = new Fatura("Cliente A", sdf.parse("20/02/2023"), 500.00);
+        Conta conta = new Conta("001", sdf.parse("20/02/2023"), 500.00, fatura);
+        Pagamento pagamento = new Pagamento(Pagamento.TipoPagamento.BOLETO, conta);
+        List<Pagamento> expected = new ArrayList<>();
+        expected.add(pagamento);
+        
+        List<Conta> contas = new ArrayList<>();
+        contas.add(conta);
+        List<String> tipos = new ArrayList<>();
+        tipos.add("boleto");
+
+        ProcessadorDeContas processador = new ProcessadorDeContas();
+        processador.processar(contas, tipos);
+        assertEquals(processador.getPagamentos(), expected);
+    }
 }
