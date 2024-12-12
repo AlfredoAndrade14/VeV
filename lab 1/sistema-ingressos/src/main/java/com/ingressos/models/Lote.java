676ee03400c7f64d78c2c40fc0b6cde1945db5d7
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-12-12 18:56:55 -0300

    feat: validando criacao de lote

--- lab 1/sistema-ingressos/src/main/java/com/ingressos/models/Lote.java
+++ lab 1/sistema-ingressos/src/main/java/com/ingressos/models/Lote.java
@@ -12,6 +12,19 @@ public class Lote {
     private List<Ingresso> ingressos;
 
     public Lote(Integer quantidade, Double percentualVip, Double precoNormal, Double desconto) {
+        if (quantidade == null || quantidade <= 0) {
+            throw new IllegalArgumentException("A quantidade de ingressos deve ser um número positivo");
+        }
+        if (percentualVip == null || percentualVip < 0.2 || percentualVip > 0.3) {
+            throw new IllegalArgumentException("O percentual de ingressos VIP deve estar entre 20% e 30%");
+        }
+        if (precoNormal == null || precoNormal <= 0) {
+            throw new IllegalArgumentException("O preço normal deve ser um número positivo");
+        }
+        if (desconto == null || desconto < 0 || desconto > 0.25) {
+            throw new IllegalArgumentException("Desconto deve estar entre 0% e 25%");
+        }
+
         this.id = contadorId++;
         this.ingressos = new ArrayList<>();
 
