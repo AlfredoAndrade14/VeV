499aeab057eaa34094f5984f9563eb409c733618
Alfredo <alfredo.andrade@ccc.ufcg.edu.br>
2024-12-12 17:19:59 -0300

    feat: add validators to pagamento object

--- lab 1/contas/src/test/java/PagamentoTest.java
+++ lab 1/contas/src/test/java/PagamentoTest.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PagamentoTest {
     private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -22,4 +23,24 @@ public class PagamentoTest {
         assertEquals(pagamento.getConta(), conta);
     }
     
+    @Test
+    public void testCreatePagamentoWithNullTipo() throws ParseException {
+        Fatura fatura = new Fatura("Cliente A", sdf.parse("20/02/2023"), 1500.00);
+        Conta conta = new Conta("001", sdf.parse("20/02/2023"), 500.00, fatura);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Pagamento(null, conta);
+        });
+
+        assertEquals("Tipo de pagamento não pode ser nulo.", exception.getMessage());
+    }
+
+    @Test
+    public void testCreatePagamentoWithNullConta() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Pagamento(Pagamento.TipoPagamento.BOLETO, null);
+        });
+
+        assertEquals("Conta não pode ser nula.", exception.getMessage());
+    }
 }
