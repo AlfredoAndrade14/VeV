ebace8449088e37cf75719756caea33422822cc0
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-12-12 17:42:40 -0300

    test: criar ingresso

---
+++ lab 1/sistema-ingressos/src/test/java/ingressoTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngressoTest {
+
+    private Ingresso ingressoVip;
+    private Ingresso ingressoNormal;
+    private Ingresso ingressoMeiaEntrada;
+
+    @BeforeEach
+    void setUp() {
+        ingressoVip = new Ingresso(TipoIngresso.VIP);
+        ingressoNormal = new Ingresso(TipoIngresso.NORMAL);
+        ingressoMeiaEntrada = new Ingresso(TipoIngresso.MEIA_ENTRADA);
+    }
+
+    @Test
+    void testCriarIngresso() {
+        assertEquals(TipoIngresso.VIP, ingressoVip.getTipo());
+        assertFalse(ingressoVip.isVendido());
+
+        assertEquals(TipoIngresso.NORMAL, ingressoNormal.getTipo());
+        assertFalse(ingressoNormal.isVendido());
+
+        assertEquals(TipoIngresso.MEIA_ENTRADA, ingressoMeiaEntrada.getTipo());
+        assertFalse(ingressoMeiaEntrada.isVendido());
+    }
+
+}
