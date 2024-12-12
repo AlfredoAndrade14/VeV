a76e963e69677d2c75d6aabcff5e896e91093136
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-12-12 19:25:33 -0300

    fix: teste show

--- lab 1/sistema-ingressos/src/test/java/ShowTest.java
+++ lab 1/sistema-ingressos/src/test/java/ShowTest.java
@@ -1,8 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.ingressos.models.Show;
 
 class ShowTest {
 
