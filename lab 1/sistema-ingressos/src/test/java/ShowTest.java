fcea6e8f5f78ac22190d165539afe4274dbb8d63
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-12-12 19:22:54 -0300

    test: criar show

---
+++ lab 1/sistema-ingressos/src/test/java/ShowTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ShowTest {
+
+    private Show show;
+
+    @BeforeEach
+    void setUp() {
+        show = new Show("2024-11-16", "Linkin Park", 1000.0, 2000.0, false);
+    }
+
+    @Test
+    void testCriarShow() {
+        assertEquals("2024-11-16", show.getData());
+        assertEquals("Linkin Park", show.getArtista());
+        assertEquals(1000.0, show.getCache());
+        assertEquals(2000.0, show.getDespesasInfraestrutura());
+        assertEquals(false, show.isDataEspecial());
+    }
+
+}
