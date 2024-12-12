20bd5e08c643a993d9ca0e82d95fc7be35f4de62
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2024-12-12 18:50:18 -0300

    fix: alterando criar Lote

--- lab 1/sistema-ingressos/src/main/java/com/ingressos/models/Lote.java
+++ lab 1/sistema-ingressos/src/main/java/com/ingressos/models/Lote.java
@@ -3,19 +3,39 @@ package com.ingressos.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ingressos.enums.TipoIngresso;
+
 public class Lote {
 
     private static int contadorId = 1;
     private final int id;
     private List<Ingresso> ingressos;
 
-    public Lote() {
+    public Lote(Integer quantidade, Double percentualVip, Double precoNormal, Double desconto) {
         this.id = contadorId++;
         this.ingressos = new ArrayList<>();
+
+        int qtdeVip = (int) (quantidade * percentualVip);
+        int qtdeMeia = (int) (quantidade * 0.1);
+        int qtdeNormal = quantidade - qtdeVip - qtdeMeia;
+
+        for (int i = 0; i < qtdeVip; i++) {
+            ingressos.add(new Ingresso(TipoIngresso.VIP));
+        }
+        for (int i = 0; i < qtdeMeia; i++) {
+            ingressos.add(new Ingresso(TipoIngresso.MEIA_ENTRADA));
+        }
+        for (int i = 0; i < qtdeNormal; i++) {
+            ingressos.add(new Ingresso(TipoIngresso.NORMAL));
+        }
     }
 
     public int getId() {
         return id;
+    }
+
+    public List<Ingresso> getIngressos() {
+        return ingressos;
     }
 
 }
