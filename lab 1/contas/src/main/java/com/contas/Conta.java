d1eac4ae980ea6ba59fc2e1bfb7f0a4b457b5147
Alfredo <alfredo.andrade@ccc.ufcg.edu.br>
2024-12-12 06:56:45 -0300

    fix: change relation

--- lab 1/contas/src/main/java/com/contas/Conta.java
+++ lab 1/contas/src/main/java/com/contas/Conta.java
@@ -6,11 +6,13 @@ public class Conta {
     private String codigo;
     private Date data;
     private double valorPago;
+    private Fatura fatura;
 
-    public Conta(String codigo, Date data, double valorPago) {
+    public Conta(String codigo, Date data, double valorPago, Fatura fatura) {
         this.codigo = codigo;
         this.data = data;
         this.valorPago = valorPago;
+        this.fatura = fatura;
     }
 
     public String getCodigo() {
@@ -23,5 +25,9 @@ public class Conta {
 
     public double getValorPago() {
         return valorPago;
+    }
+
+    public Fatura geFatura() {
+        return fatura;
     }
 }
